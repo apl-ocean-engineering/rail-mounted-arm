670d18f7d337d3d7c89c06ec9d74e2ce1a67dbcf
Stefan Layanto <stef.layanto@gmail.com>
2018-12-04 11:12:41 -0800

    added plotter functionality

--- Adafruit_Pot/Adafruit_Pot.ino
+++ Adafruit_Pot/Adafruit_Pot.ino
@@ -1,7 +1,9 @@
 #if defined(ARDUINO_SAMD_ZERO) && defined(SERIAL_PORT_USBVIRTUAL)
-  // Required for Serial on Zero based boards
-  #define Serial SERIAL_PORT_USBVIRTUAL
+// Required for Serial on Zero based boards
+#define Serial SERIAL_PORT_USBVIRTUAL
 #endif
+
+boolean serialPlotter = false;
 
 // Light One:    Analog in: A0      PWM Out: 13  (pin 13 also has RED LED)
 // Light Two:    Analog in: A1      PWM out: 12
@@ -22,7 +24,7 @@ void setup() {
 
   pinMode(L2PWM, OUTPUT);
   digitalWrite(L2PWM, HIGH);
-  
+
   Serial.begin(9600);
 }
 
@@ -30,26 +32,30 @@ void setup() {
 void loop() {
   control_pwm( analogRead(L1IN), L1PWM );
   control_pwm( analogRead(L2IN), L2PWM );
-
+  Serial.println("");
   // No need to update any faster than a few Hz
   delay(100);
 }
 
 
 void control_pwm( int ain, int pwm_pin ) {
-  
+
   int pwm = map(ain, 0, 1023, 255, 0);
 
-  
-  if( pwm >= 254 ) {
+  if ( pwm >= 254 ) {
     digitalWrite( pwm_pin, HIGH );
-  } else if( pwm <= 2 ) {
+  } else if ( pwm <= 2 ) {
     digitalWrite( pwm_pin, LOW );
   } else {
     analogWrite( pwm_pin, pwm );
   }
-  
-  Serial.print(pwm_pin);
-  Serial.print(" : ");
-  Serial.println(pwm);
+  if (serialPlotter) {
+    Serial.print(pwm);
+    Serial.print(", ");
+  } else {
+    Serial.print(pwm_pin);
+    Serial.print(" : ");
+    Serial.print(pwm);
+    Serial.print("\t\t");
+  }
 }
